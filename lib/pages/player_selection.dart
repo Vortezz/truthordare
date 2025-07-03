5b3ae06216538c6e22c50efff44547cf4dc0876d
Vortezz <contact@vortezz.dev>
2025-07-03 18:27:26 +0200

    fix: bump target api version

--- lib/pages/player_selection.dart
+++ lib/pages/player_selection.dart
@@ -3,8 +3,8 @@ import 'package:flutter_vortezz_base/components/button.dart';
 import 'package:flutter_vortezz_base/components/text.dart';
 import 'package:truthordare/pages/add_player.dart';
 import 'package:truthordare/pages/game.dart';
-import 'package:truthordare/struct/tord_client.dart';
 import 'package:truthordare/struct/player.dart';
+import 'package:truthordare/struct/tord_client.dart';
 
 class PlayerSelectionPage extends StatefulWidget {
   const PlayerSelectionPage({
@@ -49,7 +49,10 @@ class _PlayerSelectionPageState extends State<PlayerSelectionPage> {
       body: Container(
         padding: EdgeInsets.only(
           top: AppBar().preferredSize.height +
-              MediaQuery.of(context).viewPadding.top,
+              MediaQuery
+                  .of(context)
+                  .viewPadding
+                  .top,
           left: 10,
           right: 10,
         ),
@@ -114,90 +117,96 @@ class _PlayerSelectionPageState extends State<PlayerSelectionPage> {
                     ),
                   players.isEmpty
                       ? Container(
+                    margin: const EdgeInsets.only(
+                      top: 20,
+                      bottom: 20,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
                           margin: const EdgeInsets.only(
-                            top: 20,
-                            bottom: 20,
+                            right: 16,
                           ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Container(
-                                margin: const EdgeInsets.only(
-                                  right: 16,
-                                ),
-                                child: const Icon(
-                                  Icons.error_outline,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              CustomText(
-                                text: client
-                                    .translate("player_selection.no_player"),
-                                client: client,
-                                textType: TextType.text,
-                                color: Colors.white,
-                              ),
-                            ],
-                          ),
-                        )
-                      : Container(
-                          margin: const EdgeInsets.only(
-                            top: 20,
-                            bottom: 20,
-                          ),
-                          width: MediaQuery.of(context).size.width * 0.8,
-                          height: MediaQuery.of(context).size.height *
-                              (category == "extreme" ? 0.3 : 0.5),
-                          child: ListView.builder(
-                            padding: const EdgeInsets.all(0),
-                            itemCount: players.length,
-                            shrinkWrap: true,
-                            controller: _scrollController,
-                            itemBuilder: (context, index) {
-                              Player player = players[index];
-
-                              return Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Text(
-                                        player.gender.icon,
-                                        style: TextStyle(
-                                          fontSize: 24,
-                                        ),
-                                      ),
-                                      Container(
-                                        margin: const EdgeInsets.only(
-                                          left: 16,
-                                        ),
-                                        child: CustomText(
-                                          text: player.name,
-                                          client: client,
-                                          textType: TextType.subtitle,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  IconButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        players.removeAt(index);
-                                      });
-                                    },
-                                    icon: const Icon(
-                                      Icons.close_rounded,
-                                      color: Colors.white,
-                                      size: 32,
-                                    ),
-                                  ),
-                                ],
-                              );
-                            },
+                          child: const Icon(
+                            Icons.error_outline,
+                            color: Colors.white,
                           ),
                         ),
+                        CustomText(
+                          text: client
+                              .translate("player_selection.no_player"),
+                          client: client,
+                          textType: TextType.text,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  )
+                      : Container(
+                    margin: const EdgeInsets.only(
+                      top: 20,
+                      bottom: 20,
+                    ),
+                    width: MediaQuery
+                        .of(context)
+                        .size
+                        .width * 0.8,
+                    height: MediaQuery
+                        .of(context)
+                        .size
+                        .height *
+                        (category == "extreme" ? 0.3 : 0.5),
+                    child: ListView.builder(
+                      padding: const EdgeInsets.all(0),
+                      itemCount: players.length,
+                      shrinkWrap: true,
+                      controller: _scrollController,
+                      itemBuilder: (context, index) {
+                        Player player = players[index];
+
+                        return Row(
+                          mainAxisAlignment:
+                          MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  player.gender.icon,
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                  ),
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.only(
+                                    left: 16,
+                                  ),
+                                  child: CustomText(
+                                    text: player.name,
+                                    client: client,
+                                    textType: TextType.subtitle,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  players.removeAt(index);
+                                });
+                              },
+                              icon: const Icon(
+                                Icons.close_rounded,
+                                color: Colors.white,
+                                size: 32,
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
                 ],
               ),
               Container(
@@ -213,34 +222,35 @@ class _PlayerSelectionPageState extends State<PlayerSelectionPage> {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => AddPlayerPage(
-                                client: client,
-                                category: category,
-                                onAddPlayer: (Player player) {
-                                  if (players
-                                      .map((e) => e.name)
-                                      .contains(player.name)) {
-                                    return false;
-                                  }
+                              builder: (context) =>
+                                  AddPlayerPage(
+                                    client: client,
+                                    category: category,
+                                    onAddPlayer: (Player player) {
+                                      if (players
+                                          .map((e) => e.name)
+                                          .contains(player.name)) {
+                                        return false;
+                                      }
 
-                                  setState(() {
-                                    players.add(player);
-                                  });
+                                      setState(() {
+                                        players.add(player);
+                                      });
 
-                                  if (_scrollController.hasClients) {
-                                    _scrollController.animateTo(
-                                      _scrollController
+                                      if (_scrollController.hasClients) {
+                                        _scrollController.animateTo(
+                                          _scrollController
                                               .position.maxScrollExtent +
-                                          50,
-                                      duration:
+                                              50,
+                                          duration:
                                           const Duration(milliseconds: 500),
-                                      curve: Curves.ease,
-                                    );
-                                  }
+                                          curve: Curves.ease,
+                                        );
+                                      }
 
-                                  return true;
-                                },
-                              ),
+                                      return true;
+                                    },
+                                  ),
                             ),
                           );
                         });
@@ -254,13 +264,14 @@ class _PlayerSelectionPageState extends State<PlayerSelectionPage> {
                         Navigator.pushAndRemoveUntil(
                           context,
                           MaterialPageRoute(
-                            builder: (BuildContext context) => GamePage(
-                              players: players,
-                              category: category,
-                              client: client,
-                            ),
+                            builder: (BuildContext context) =>
+                                GamePage(
+                                  players: players,
+                                  category: category,
+                                  client: client,
+                                ),
                           ),
-                          (route) => false,
+                              (route) => false,
                         );
                       },
                       client: client,
